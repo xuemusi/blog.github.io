7727f7b5d357293aa8c5d2576b0214ed21e63385
“huangqs” <huangqs@mingyuanyun.com>
2025-02-23 15:58:30 +0800

    Site updated: 2025-02-23 15:58:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/23/全流程指南，免费部署超/","2025/02/23/PC住宅代理如何使用/"];function toRandomPost(){
+var posts=["2025/02/23/全流程指南，免费部署超/","2025/02/23/雪慕斯的频道/","2025/02/23/PC住宅代理如何使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
